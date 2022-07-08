13c0650d08ea3aae653a248fbd283b8fea0e102c
luke <luke.fun59@gmail.com>
2022-07-08 16:50:28 -0700

    FORLOOPHELL

---
+++ src/_02_nested_loops/_3_for_loop_gauntlet/For_Loop_Guantlet_Of_Extreme_Pain_And_Suffering.java
@@ -0,0 +1,36 @@
+package _02_nested_loops._3_for_loop_gauntlet;
+
+public class For_Loop_Guantlet_Of_Extreme_Pain_And_Suffering {
+public static void main(String[] args) {
+	//SINGLE LOOPS
+	//for(int i = 0; i <= 100; i ++) {
+		//System.out.println(i);
+	//}
+//for(int i = 100; i >= 0; i --) {
+		//System.out.println(i);
+	//}
+	//for(int i = 2; i <= 100; i += 2) {
+		//System.out.println(i);
+	//}
+	//for(int i = 1; i <= 99; i += 2) {
+		//System.out.println(i);
+	//}
+	//for(int i = 1; i <= 500; i ++) {
+		//if(i % 2 == 0) {
+		//System.out.println(i + " is even");
+		//}else {
+			//System.out.println(i + " is odd");
+		//}
+	//}
+	//for(int i = 7; i <= 777; i += 7) {
+		//System.out.println(i);
+	//}
+	//int year = 2010;
+	//for(int i = 0; i <= 12; i ++) {
+		//System.out.println("in " + year + ", I was " + i + " years old.");
+		//year ++;
+	//}
+	//NESTED LOOPS
+	
+}
+}
