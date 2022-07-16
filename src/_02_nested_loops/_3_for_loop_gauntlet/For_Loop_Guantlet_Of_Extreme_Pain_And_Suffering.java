0883a78a406513bebf9cdbffdda1dfdaf48dac61
root <root@62483ebe6b35>
2022-07-16 18:24:06 +0000

    For Loop Guantlet Of EXTREME Pain And Suffering

--- src/_02_nested_loops/_3_for_loop_gauntlet/For_Loop_Guantlet_Of_Extreme_Pain_And_Suffering.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/For_Loop_Guantlet_Of_Extreme_Pain_And_Suffering.java
@@ -38,18 +38,35 @@ public static void main(String[] args) {
 				//i2 = 0;
 			//}
 		//}
-	for(int i = 1; i <= 7; i += 3) {
-		for(int i2 = 2; i2 <= 8; i2 += 3 ) {
-		
-			System.out.println(i + " " +i2);
-		
+	//int i3 = 3;
+	//for(int i = 1; i <= 7; i += 0) {
+		//for(int i2 = 2; i2 <= 8; i2 += 3 ) {
+			//System.out.println(i + " " +i2+ " " + i3);
+			//i += 3;
+			//i3 += 3;
+		//}
+	//}
+	//int i3 = 1;
+		//for(int i = 1; i <= 10; i ++) {
+			//for(int i2 = 1; i2 <= 10; i2 += 1) {
+				//System.out.print(i3 + " ");
+				//i3 += 1;
+			//}
+			//System.out.println();
+		//}
+	//for(int i = 0; i < 6; i ++) {
+		//for(int i2 = 0; i2 <= i; i2 ++) {
+			//System.out.print("* ");
+		//}
+			//System.out.println();
+	//}
+	for(int i = 0; i <= 100; i ++) {
+		System.out.print(100 - i +  " ");
+		if(i % 10 == 0) {
+			System.out.println();
 		}
 	}
-		  
-			
 	
-		
-	}
-	
+}
 }
 
