dcd79396006cd7d0db5cdbf06b8d2fcbcb54e038
root <root@e23aba17dd1b>
2022-07-30 18:31:15 +0000

    Obedient Robot, Part of Happy Pet

--- src/_03_method_writing/_1_obedient_robot/OBEY.java
+++ src/_03_method_writing/_1_obedient_robot/OBEY.java
@@ -1,10 +1,14 @@
 package _03_method_writing._1_obedient_robot;
 
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class OBEY {
-	Robot bob = new Robot();
-	public void drawSquare() {
+	static Robot bob = new Robot();
+	public static void drawSquare() {
 		bob.penDown();
 		bob.setSpeed(50);
 		bob.move(250);
@@ -16,7 +20,7 @@ public class OBEY {
 		bob.move(250);
 		bob.turn(90);	
 	}
-	public void drawTriangle() {
+	public static void drawTriangle() {
 		bob.penDown();
 		bob.setSpeed(50);
 		bob.move(250);
@@ -26,7 +30,7 @@ public class OBEY {
 		bob.move(250);
 		bob.turn(120);
 	}
-	public void drawCircle() {
+	public static void drawCircle() {
 		bob.penDown();
 		bob.setSpeed(50);
 		for(int i = 0; i <= 360; i ++) {
@@ -35,7 +39,25 @@ public class OBEY {
 		}
 	}
 	public static void main(String[] args) {
-		OBEY bobby = new OBEY();
-		bobby.drawCircle();
+		OBEY bobby = new OBEY(); 
+		 String question1 = JOptionPane.showInputDialog(null, "What shape would you like me to draw? (square, triangle, or circle)");
+		 String question2 = JOptionPane.showInputDialog(null, "What color " + question1 + " would you like me to draw? (red, green, blue, default)");
+		 if(question2.equals("red")) {
+			bob.setPenColor(Color.red);
+		 }else if(question2.equals("green")){
+			 bob.setPenColor(Color.green);
+		 }else if(question2.equals("blue")) {
+			 bob.setPenColor(Color.blue);
+		 }else {
+			 bob.setPenColor(Color.black);
+		 }
+		 if(question1.equals("square")) {
+			 drawSquare();
+		 }else if(question1.equals("triangle")){
+			 drawTriangle();
+		 }else if(question1.equals("circle")) {
+			 drawCircle();
+		 }
 	}
+	
 }
--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -2,26 +2,50 @@ package _03_method_writing._2_happy_pet;
 import javax.swing.JOptionPane;
 
 public class HappyPet {
-	
+	public static void cuddle(String pet) {	
+		if(pet.equals("dog")) {
+			JOptionPane.showMessageDialog(null, "Your dog loves cuddling with you!");
+			happinessLevel += 10;
+		}else if(pet.equals("cat")) {
+			JOptionPane.showMessageDialog(null, "Your cat enjoys cuddling with you.");
+			happinessLevel += 5;
+		}else if(pet.equals("hamster")) {
+		JOptionPane.showMessageDialog(null, "You squeeze your hamster so hard it flys into the air and lands on a soft cushion. (It does not look happy)");
+		happinessLevel += -5;
+	}
+	}
+	public static void food() {
+	}
+	public static void water() {	
+	}
+	public static void takeAWalk() {
+	}
+	public static void groom() {
+	}
+	public static void cleanUpPoop() {	
+	}
 	// 1. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	static int happinessLevel = 0;
 	public static void main(String[] args) {
 		// 2. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+		String question1 = JOptionPane.showInputDialog(null, "What kind of pet do you want? (dog, cat, hamster, duck, or fish)");
 		// 3. REPEAT steps 4 - 7 enough times to make your pet happy!
-		
+		for(int turns = 0; happinessLevel <= 100; turns ++) {
 			// 4. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
-
+			int task = JOptionPane.showOptionDialog(null, "What would you like to do with your " + question1 + "?", "Do something with your new pet!", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Cuddle", "Food", "Water", "Take a walk", "Groom", "Clean up poop" }, null);
+					if(task == 0) {
+						cuddle(question1);
+					}
 			// 6. Use user input to call the appropriate method created in step 5 below.
 
 			// 7. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
+		
+		}
 	}
 
 	// 5. Create methods to handle each of your user selections.
