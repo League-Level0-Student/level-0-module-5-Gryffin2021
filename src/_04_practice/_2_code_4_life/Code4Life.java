2842235b5e2130d0c3474e4b4d1757ff63022329
root <root@0ab386c337da>
2022-08-20 18:26:45 +0000

    Code4Life, Self Test = ForLoopOnslaught

--- src/_04_practice/_2_code_4_life/Code4Life.java
+++ src/_04_practice/_2_code_4_life/Code4Life.java
@@ -14,22 +14,30 @@ public class Code4Life {
 	 * If you are not sure, ask your teacher 
 	 * *****************/
 	static boolean canPlaySounds = true;
+	public static void main(String[] args) {
+		
 	
 	
 	// 1. Ask the user how many hours they spent coding this week.
-
+		String hours = JOptionPane.showInputDialog(null, "How many hours have you spent coding this week?");
+		int hours2 = Integer.parseInt(hours);
 	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
 	// write code instead.
-
+		if(hours2 <= 2) {
+			JOptionPane.showMessageDialog(null, "Stop watching YouTube and CODE!");
+		}
 	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
 	// they're a Code Ninja
-
+		if (hours2 >= 3 && hours2 <= 5) {
+			JOptionPane.showMessageDialog(null, "Wow! Your a Code Ninja!");
+		}
 	// 4. If it is more than 5, call the playBatmanTheme  method, then 
 	//    call the displayBatman method.
-
-
-
-	
+		if(hours2 > 5) {
+			playBatmanTheme();
+			displayBatman();
+		}
+	}
 	private static void playBatmanTheme() {
 	// NOTE: Download batman.wav from league-sounds on GitHub: https://github.com/jointheleague/league-sounds
 	//	     Then put batman.wav into the "_2_code_4_life" project
